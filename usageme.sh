63fcf163c95d4de3ad425be2e64937458e866e0f
Mark Williams <markwilliams@mwilliams-imac.f4tech.com>
2014-06-02 11:07:55 -0600

    Updated to use HOME_DIR

--- usageme.sh
+++ usageme.sh
@@ -9,7 +9,7 @@ SUBSCRIPTION_ID=$1
 
 MY_USERNAME="user@company.com"
 MY_PASSWORD="top\$3cr\$t"
-HOME_DIR="/Users/mydomainusername/"
+HOME_DIR="/Users/mydomainusername"
 INSTALL_DIR="${HOME_DIR}/Documents/rally-subadminme-master"
 OUTPUT_DIR="${HOME_DIR}/Desktop"
 
@@ -21,5 +21,5 @@ filename="Usage_${datetimestamp}_${1}.csv"
 
 echo "Pulling ${2}-day usage report for SubscriptionID: ${1}"
 
-${PYTHON} ${INSTALL_DIR}/usage.py -s ${SUBSCRIPTION_ID} -d $2 -u ${MY_USERNAME} -p ${MY_PASSWORD} > /Users/mydomainusername/Desktop/${filename}
+${PYTHON} ${INSTALL_DIR}/usage.py -s ${SUBSCRIPTION_ID} -d $2 -u ${MY_USERNAME} -p ${MY_PASSWORD} > ${HOME_DIR}/Desktop/${filename}
 echo "Usage report written to file: ${filename}"
